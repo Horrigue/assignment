bcdc0f6bd4f36a0c0f59bde48ee629c3c3f7f646
Horrigue <799804394@qq.com>
2024-07-14 20:55:36 +0800

    Initial commit

---
+++ Source/assignment/assignmentGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "assignmentGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AassignmentGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AassignmentGameMode();
+};
+
+
+
